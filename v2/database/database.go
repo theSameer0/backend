cd0d2fa49d6a47ab0f7bdba2265f5ee625b1838e
Sameer Mishra <sameermishra@IM-HYD-LAP-0241.local>
2023-05-09 18:13:27 +0530

    update the database location

--- v2/database/database.go
+++ v2/database/database.go
@@ -7,7 +7,8 @@ import (
 )
 
 func Connect() *sql.DB {
-	dsn := "user=sameermishra password=sameer dbname=mydatabase port=5430 sslmode=disable"
+	// dsn := "user=sameermishra password=sameer dbname=mydatabase port=5430 sslmode=disable"
+	dsn := "postgres://mbs_user:WnAOrReVtSNPp54osR2UIcdAZ9a2YgCz@dpg-cgiiid6bb6mnfcqlsb0g-a/mbs"
 	db, err := sql.Open("postgres", dsn)
 	CheckErr(err)
 	return db
